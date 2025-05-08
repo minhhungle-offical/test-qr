c29546390bcc4c55069efc6b7568a79d274daac2
minhhungle <minhhungle.offical@gmail.com>
2025-05-08 14:16:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // src/App.jsx
+import { useState } from "react";
 import QRScanner from "./components/QRScanner";
 
 function App() {
+  const [data, setData] = useState(null);
   const handleScan = (data) => {
     alert("Đã quét được mã QR: " + data);
+    setData(data);
   };
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Quét mã QR</h1>
       <QRScanner onScan={handleScan} />
+      <p>{data}</p>
     </div>
   );
 }
